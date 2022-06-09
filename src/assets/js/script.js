6810fbba93037335d3bc424374c414b4a39fd166
Ryan <62509624+ryanmathewdsouza@users.noreply.github.com>
2022-06-09 16:21:29 +0100

    plus button triggers alert message

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,8 @@
 import * as bootstrap from 'bootstrap';
+
+let current = 0;
+function addItem() {
+    alert ("Item added to order");
+}
+const plus = document.getElementById("plus");
+plus.onclick = addItem;
