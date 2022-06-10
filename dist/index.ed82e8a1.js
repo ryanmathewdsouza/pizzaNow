14815f58358c869a4c166dbe9d947d89a47139fa
Ryan <62509624+ryanmathewdsouza@users.noreply.github.com>
2022-06-10 11:17:50 +0100

    added comments

--- dist/index.ed82e8a1.js
+++ dist/index.ed82e8a1.js
@@ -508,7 +508,10 @@ let current = 0; // commented out code triggers a custom function, replaced this
  // function addItem() {
  //     alert ("Item added to order");
  // }
+ // document.getElementsByClassName()
+ // make sure to use getElementById(), not getElementsByClassName()
  // const plus = document.getElementById("plus");
+ // make sure to reference the addItem function by omitting the brackets, including them will call the function as it's read'
  // plus.onclick = addItem;
 
 },{"bootstrap":"h36JB"}],"h36JB":[function(require,module,exports) {
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,5 +6,8 @@ let current = 0;
 // function addItem() {
 //     alert ("Item added to order");
 // }
+// document.getElementsByClassName()
+// make sure to use getElementById(), not getElementsByClassName()
 // const plus = document.getElementById("plus");
+// make sure to reference the addItem function by omitting the brackets, including them will call the function as it's read'
 // plus.onclick = addItem;
