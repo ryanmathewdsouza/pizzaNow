af0aa11875731266f14a82e1715147c9e39be7ad
Ryan <62509624+ryanmathewdsouza@users.noreply.github.com>
2022-06-13 17:43:53 +0100

    First two pizzas added to basket modal in a verbose way

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,34 @@
 import * as bootstrap from 'bootstrap';
 
-let current = 0;
+let pestoAdd = document.getElementById("pesto-add");
+pestoAdd.onclick = addPesto;
+
+function addPesto() {
+    // alert("You have selected a " + document.getElementById("grilled-vegi-pesto-size").value + " sized Grilled Vegi Pesto pizza");
+    var table = document.getElementById("basket-table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = document.getElementById("grilled-vegi-pesto-size").value;
+    cell2.innerHTML = document.getElementById("grilled-vegi-pesto-crust").value;
+    cell3.innerHTML = "Grilled Vegi Pesto Pizza";
+}
+
+let bangerAdd = document.getElementById("banger-add");
+bangerAdd.onclick = addBanger;
+
+function addBanger() {
+    // alert("You have selected a " + document.getElementById("grilled-vegi-pesto-size").value + " sized Grilled Vegi Pesto pizza");
+    var table = document.getElementById("basket-table");
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = document.getElementById("absolute-banger-size").value;
+    cell2.innerHTML = document.getElementById("absolute-banger-crust").value;
+    cell3.innerHTML = "Absolute Banger Pizza";
+}
 
 // commented out code triggers a custom function, replaced this for bootstrap modal code
 // function addItem() {
