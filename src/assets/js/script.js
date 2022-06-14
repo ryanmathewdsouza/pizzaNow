b254598e89ce51f46e3cf3b11ef7dfe03ddeab23
Ryan <62509624+ryanmathewdsouza@users.noreply.github.com>
2022-06-14 17:44:08 +0100

    First row of pizzas are getting added to basket, with prices

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -23,18 +23,55 @@ let bangerAdd = document.getElementById("banger-add");
 bangerAdd.onclick = addBanger;
 
 function addBanger() {
-    const table = document.getElementById("basket-table");
+    let table = document.getElementById("basket-table");
+    let values = document.getElementById("absolute-banger-size");
+    let feedArray = values.value.split(",");
     let row = table.insertRow(-1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
-    cell1.innerHTML = document.getElementById("absolute-banger-size").value;
+    cell1.innerHTML = feedArray[0];
     cell2.innerHTML = document.getElementById("absolute-banger-crust").value;
-    cell3.innerHTML = $("absolute-banger-size option:selected").data('name');
-    cell4.innerHTML = $("absolute-banger-size option:selected").data('price');
+    cell3.innerHTML = feedArray[1];
+    cell4.innerHTML = feedArray[2];
 }
 
+let theCheeseburgerAdd = document.getElementById("the-cheeseburger-add");
+theCheeseburgerAdd.onclick = addTheCheeseburger;
+
+function addTheCheeseburger() {
+    let table = document.getElementById("basket-table");
+    let values = document.getElementById("the-cheeseburger-size");
+    let feedArray = values.value.split(",");
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = feedArray[0];
+    cell2.innerHTML = document.getElementById("the-cheeseburger-crust").value;
+    cell3.innerHTML = feedArray[1];
+    cell4.innerHTML = feedArray[2];
+}
+
+let theMeatfielderAdd = document.getElementById("the-meatfielder-add");
+theMeatfielderAdd.onclick = addTheMeatfielder;
+
+function addTheMeatfielder() {
+    let table = document.getElementById("basket-table");
+    let values = document.getElementById("the-meatfielder-size");
+    let feedArray = values.value.split(",");
+    let row = table.insertRow(-1);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = feedArray[0];
+    cell2.innerHTML = document.getElementById("the-meatfielder-crust").value;
+    cell3.innerHTML = feedArray[1];
+    cell4.innerHTML = feedArray[2];
+}
 // commented out code triggers a custom function, replaced this for bootstrap modal code
 // function addItem() {
 //     alert ("Item added to order");
